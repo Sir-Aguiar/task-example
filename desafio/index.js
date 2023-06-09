f93efa91aa48a87a117f8ee263ee2927f10ba24d
Sir-Aguiar <felipeaguiarficdev@gmail.com>
2023-06-09 10:52:41 -0400

    Starting basic

---
+++ desafio/index.js
@@ -0,0 +1,7 @@
+/* 
+Criar um programa para gerenciamento de tarefas, utilizando a arquitetura MVC e que tenha os seguintes
+requisitos: 
+  - tenha um menu onde o usuário pode escolher (criar tarefa, listar tarefas, deletar tarefa e sair do programa);
+  - seu programa deve atender todas essas funcionalidades. 
+*/
+
